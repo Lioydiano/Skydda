0aab74e83ac338974eade28592f40a56c57d15d0
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2023-01-02 15:21:33 +0100

    Componente - get/set

--- include/skydda/componente.cpp
+++ include/skydda/componente.cpp
@@ -4,4 +4,22 @@
 namespace skydda {
     Componente::Componente() : carattere(), coordinate(), stile() {}
     Componente::Componente(char carattere_, Coordinate coordinata_, ANSI::Stile stile_) : carattere(carattere_), coordinate(coordinata_), stile(stile_) {}
+    char Componente::getCarattere() const {
+        return carattere;
+    }
+    void Componente::setCarattere(char carattere_) {
+        carattere = carattere_;
+    }
+    Coordinate Componente::getCoordinate() const {
+        return coordinate;
+    }
+    void Componente::setCoordinate(Coordinate coordinate_) {
+        coordinate = coordinate_;
+    }
+    ANSI::Stile Componente::getStile() const {
+        return stile;
+    }
+    void Componente::setStile(ANSI::Stile stile_) {
+         stile = stile_;
+    }
 }
--- include/skydda/componente.hpp
+++ include/skydda/componente.hpp
@@ -12,8 +12,15 @@ namespace skydda {
         ANSI::Stile stile;
     
     public:
-        Componente() : coordinate(), stile(), carattere() {}
+        Componente();
         Componente(char, Coordinate, ANSI::Stile);
         virtual ~Componente() {} // Distruttore virtuale (https://github.com/FLAK-ZOSO/Sista/issues/2) (https://stackoverflow.com/questions/10338548/making-a-vector-of-instances-of-different-subclasses)
+
+        char getCarattere() const;
+        void setCarattere(char);
+        Coordinate getCoordinate() const;
+        void setCoordinate(Coordinate);
+        ANSI::Stile getStile() const;
+        void setStile(ANSI::Stile);
     };
 };
