b62479f2b27eae9af5865c12d6f6cb3f90055a7d
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2023-01-04 21:50:34 +0100

    Nemico, Difensore, Proiettile - Implementazione costruttori
    @Elisssx

--- include/skydda/oggetti.cpp
+++ include/skydda/oggetti.cpp
@@ -18,6 +18,51 @@ namespace skydda {
         {SUD, 'v'},
         {OVEST, '<'}
     };
+    ANSI::Stile stileProiettileDifensore(
+        ANSI::ColoreTesto::MAGENTA,
+        ANSI::ColoreSfondo::ROSSO,
+        ANSI::Attributo::LUMINOSO
+    );
+    ANSI::Stile stileProiettileNemico(
+        ANSI::ColoreTesto::CIANO,
+        ANSI::ColoreSfondo::NERO,
+        ANSI::Attributo::LUMINOSO
+    );
+    std::unordered_map<TipoProiettile, ANSI::Stile> stiliProiettile = {
+        {DIFENSORE, stileProiettileDifensore},
+        {NEMICO, stileProiettileNemico}
+    };
+
+    ANSI::Stile stileNemico(
+        ANSI::ColoreTesto::BLU,
+        ANSI::ColoreSfondo::NERO,
+        ANSI::Attributo::LUMINOSO
+    );
+    Nemico::Nemico() : Componente('1', Coordinate(0, 0), stileNemico), vita(1) {} 
+    Nemico::Nemico(char carattere_, Coordinate coordinate_, short int vita_) : Componente(carattere_, coordinate_, stileNemico), vita(vita_) {}
+    Nemico::~Nemico() {}
+    void Nemico::setVita(short int vita_) {
+        vita = vita_;
+    }
+    short int Nemico::getVita() const {
+        return vita;
+    }
+
+
+    ANSI::Stile stileDifensore(
+        ANSI::ColoreTesto::VERDE,
+        ANSI::ColoreSfondo::NERO,
+        ANSI::Attributo::LUMINOSO
+    );
+    Difensore::Difensore(): Componente('1', Coordinate(0, 0), stileDifensore) {} 
+    Difensore::~Difensore() {}
+    void Difensore::stampa() {
+        std::cout << u8"安";
+    }
+
+    Proiettile::Proiettile() : Componente(direzioneCarattere[EST], Coordinate(0, 0), stileProiettileDifensore), origine(DIFENSORE), direzione(EST), velocita(1) {}
+    Proiettile::Proiettile(Coordinate coordinate_, TipoProiettile tipo_, Direzione direzione_, int velocita_) : Componente(direzioneCarattere[direzione_], coordinate_, stiliProiettile[tipo_]), origine(tipo_), direzione(direzione_), velocita(velocita_) {}
+    Proiettile::~Proiettile() {}
 
     ANSI::Stile stileTerreno(
         ANSI::ColoreTesto::ROSSO,
