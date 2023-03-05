b78b63ab669821064c6bb23ccee0aed5a988fe68
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2023-03-05 18:23:47 +0100

    "Terreno" made out of 'T'

--- terreno.cpp
+++ terreno.cpp
@@ -2,7 +2,7 @@
 
 namespace skydda {
     ANSI::Stile stileTerreno(ANSI::ColoreTesto::ROSSO, ANSI::ColoreSfondo::S_ROSSO, ANSI::Attributo::FLEBILE);
-    Terreno::Terreno() : Componente(' ', Coordinate(0, 0), stileTerreno, TipoComponente::TERRENO) {}
-    Terreno::Terreno(Coordinate coordinate_) : Componente(' ', coordinate_, stileTerreno, TipoComponente::TERRENO) {}
+    Terreno::Terreno() : Componente('T', Coordinate(0, 0), stileTerreno, TipoComponente::TERRENO) {}
+    Terreno::Terreno(Coordinate coordinate_) : Componente('T', coordinate_, stileTerreno, TipoComponente::TERRENO) {}
     Terreno::~Terreno() {}
 }
